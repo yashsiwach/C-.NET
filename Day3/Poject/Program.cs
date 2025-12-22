0803aaea788f87d9eb51e2b803d1c0bdec726e36
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2025-12-22 09:50:11 +0530

    Add comprehensive comments to Program.cs

--- Day3/Poject/Program.cs
+++ Day3/Poject/Program.cs
@@ -1,30 +1,54 @@
-﻿using System;
+using System;
 using Game;
 using System.Collections;
 using System.Runtime.InteropServices;
+
 public class Program
 {
     public static void Main(String[] args)
     {
-        int n=int.Parse(Console.ReadLine());
-        Employee [] data=new Employee[n];
+        // Read the number of employees from user input
+        int n = int.Parse(Console.ReadLine());
+        
+        // Create an array to store Employee objects
+        Employee[] data = new Employee[n];
+        
+        // Declare variables to store employee data
         int id;
         string name;
         bool isPlaying;
         int projectsDone;
         int timeTaken;
+        
+        // Loop through to collect data for each employee
         for(int i = 0; i < n; i++)
         {
-            id=int.Parse(Console.ReadLine());
+            // Read employee ID
+            id = int.Parse(Console.ReadLine());
+            
+            // Read employee name
             name = Console.ReadLine();
-            isPlaying=bool.Parse(Console.ReadLine());
-            projectsDone=int.Parse(Console.ReadLine());
-            timeTaken=int.Parse(Console.ReadLine());
-            Employee obj=new Employee(id,name,isPlaying,projectsDone,timeTaken);
-            data[i]=obj;
+            
+            // Read playing status (true/false)
+            isPlaying = bool.Parse(Console.ReadLine());
+            
+            // Read number of projects completed
+            projectsDone = int.Parse(Console.ReadLine());
+            
+            // Read time taken for projects
+            timeTaken = int.Parse(Console.ReadLine());
+            
+            // Create new Employee object with the collected data
+            Employee obj = new Employee(id, name, isPlaying, projectsDone, timeTaken);
+            
+            // Store the employee object in the array
+            data[i] = obj;
         }
         
+        // Create a Result object to process the employee data
         Result result = new Result();
+        
+        // Call the Playing method to process employees who are playing
         result.Playing(data, n);
     }
 }
