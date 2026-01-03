e1bf853edd1a46c894ade49e49675b3b18dee3fe
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-01-03 14:29:50 +0530

    Partial class

--- Projects /ScenrioProject4 -PayRollPro/FullTimeEmployee.cs
+++ Projects /ScenrioProject4 -PayRollPro/FullTimeEmployee.cs
@@ -1,24 +1,59 @@
 namespace ScenrioProject4.PayRollPro
 {
+    #region Full-Time Employee Class
+
+    /// <summary>
+    /// FullTimeEmployee represents a permanent employee
+    /// whose monthly pay includes hourly wages
+    /// along with a fixed monthly bonus.
+    /// </summary>
     public class FullTimeEmployee : EmployeeRecord
     {
-        public double HourlyRate{get;set;}
-        public double MonthlyBonus{get;set;}
-        public FullTimeEmployee(string name,double[] arr,double HourlyRate,double MonthlyBonus)
+        #region Data Members
+
+        public double HourlyRate { get; set; }
+        public double MonthlyBonus { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public FullTimeEmployee(
+            string name,
+            double[] arr,
+            double HourlyRate,
+            double MonthlyBonus
+        )
         {
-            this.EmployeeName=name;
-            this.WeeklyHours=arr;
-            this.HourlyRate=HourlyRate;
-            this.MonthlyBonus=MonthlyBonus;
+            this.EmployeeName = name;
+            this.WeeklyHours = arr;
+            this.HourlyRate = HourlyRate;
+            this.MonthlyBonus = MonthlyBonus;
         }
+
+        #endregion
+
+        #region Overridden Methods
+
+        /// <summary>
+        /// Calculates monthly pay by summing
+        /// weekly hours, multiplying by hourly rate,
+        /// and adding the monthly bonus.
+        /// </summary>
         public override double GetMonthlyPay()
         {
-            double sum=0;
-            foreach(var i in WeeklyHours)
+            double sum = 0;
+
+            foreach (var i in WeeklyHours)
             {
-                sum+=i;
+                sum += i;
             }
-            return ((sum*HourlyRate)+MonthlyBonus);
+
+            return ((sum * HourlyRate) + MonthlyBonus);
         }
+
+        #endregion
     }
+
+    #endregion
 }
