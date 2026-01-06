de09dc44b4aedbe5fd3b8924f1a0192abe48ecc9
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-01-06 18:35:12 +0530

    Day 9 Done

---
+++ Day9 -XML/Delegates/Program.cs
@@ -0,0 +1,74 @@
+﻿using Del;
+
+/// <summary>
+/// Demonstrates delegate usage in C#
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Delegate that references methods taking two integers
+    /// and returning an integer
+    /// </summary>
+    public delegate int DelFunciton(int a, int b);
+
+    /// <summary>
+    /// Entry point of the program
+    /// </summary>
+    public static void Main()
+    {
+        Program obj = new Program();
+        Student obj1 = new Student();
+
+        // Assigning Add1 method to Student delegate
+        Student.StuDelegate d1 = obj.Add1;
+
+        // Invoking delegate
+        int r1 = d1(5, 3);
+        System.Console.WriteLine(r1);
+
+        // Creating delegate instance using another method
+        DelFunciton myDel = new DelFunciton(obj.Add2);
+
+        // Invoking delegate
+        int result = myDel(1, 2);
+        System.Console.WriteLine(result);
+    }
+
+    /// <summary>
+    /// Adds two numbers and an extra value of 10
+    /// </summary>
+    public int Add1(int a, int b)
+    {
+        return a + b + 10;
+    }
+
+    /// <summary>
+    /// Adds two numbers and an extra value of 20
+    /// </summary>
+    public int Add2(int a, int b)
+    {
+        return a + b + 20;
+    }
+}
+
+namespace Del
+{
+    /// <summary>
+    /// Student class containing its own delegate
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Delegate for student operations
+        /// </summary>
+        public delegate int StuDelegate(int a, int b);
+
+        /// <summary>
+        /// Subtracts two numbers
+        /// </summary>
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
--- Day9 -XML/XmlConvertor/Program.cs
+++ Day9 -XML/XmlConvertor/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Runtime.Serialization.Formatters.Binary;
+﻿using System.Diagnostics;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
 using System.Xml.Serialization;
+using System.Diagnostics;
 
 public class Program
 {
@@ -11,26 +14,33 @@ public class Program
         public int Id { get; set; }
         [XmlAttribute]
         public string? Name { get; set; }
-        public List<int> data = new List<int> { 5, 7, 1, 2, 3 };
-        //  public Dictionary<string,int>dict=new Dictionary<string, int>();
+        public List<int> data { get; set; } = new List<int> { 5, 7, 1, 2, 3 };
+        public Dictionary<string, int> dict { get; set; } = new Dictionary<string, int>();
         [XmlIgnore]
         public List<string> data1 = new List<string> { "test1", "test2" };
 
     }
+    public class ProcessInfo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public long Memory { get; set; }
+    }
+
     public static void Main()
     {
         Student stu = new Student();
         stu.Id = 2;
         stu.Name = "test";
-        //  stu.dict["ok"]=3;
-        XmlSerializer serializer = new XmlSerializer(typeof(Student));
-        StringWriter sw = new StringWriter();
-        serializer.Serialize(sw, stu);
-        string xml = sw.ToString();
-        System.Console.WriteLine(xml);
+        stu.dict["ok"] = 3;
+        // XmlSerializer serializer = new XmlSerializer(typeof(Student));
+        // StringWriter sw = new StringWriter();
+        // serializer.Serialize(sw, stu);
+        // string xml = sw.ToString();
+        // System.Console.WriteLine(xml);
 
         //Binary formatter has been discontinued due to security resons
-        
+
         // #pragma warning disable SYSLIB0011
         // BinaryFormatter bf = new BinaryFormatter();
         // MemoryStream ms = new MemoryStream();
@@ -38,6 +48,37 @@ public class Program
         // byte[] bin = ms.ToArray();
         // System.Console.WriteLine(bin.Length);
         // #pragma warning restore SYSLIB0011
+
+        //json serialization
+       // string json = JsonSerializer.Serialize(stu);
+       // System.Console.WriteLine(json);
+        //json deserialization
+        //Student? deObj = JsonSerializer.Deserialize<Student>(json);
+        //System.Console.WriteLine(deObj.Name);
+
+        Process[] processes = Process.GetProcessesByName("Ulaa");
+
+        List<ProcessInfo> list = new List<ProcessInfo>();
+
+        foreach (Process p in processes)
+        {
+            list.Add(new ProcessInfo
+            {
+                Id = p.Id,
+                Name = p.ProcessName,
+                Memory = p.WorkingSet64
+            });
+        }
+        string json = JsonSerializer.Serialize(list);
+        System.Console.WriteLine(json);
+
+
+        List<ProcessInfo> deArr=JsonSerializer.Deserialize<List<ProcessInfo>>(json);
+        foreach(var i in deArr)
+        {
+            System.Console.WriteLine(i.Name);
+        }
+
 
     }
 }
