a4137da66ec992a3dd9690f423a492ee856b8a3f
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-01-19 10:28:28 +0530

    Task 2 Done!

--- Projects /TopBrains Ex/Task2/Program.cs
+++ Projects /TopBrains Ex/Task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// <summary>
+/// Entry point of the program.
+/// Reads a number and a limit, generates the multiplication table
+/// of the given number up to the specified limit, and prints it.
+/// </summary>
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Read the number whose table is needed
+        int n = int.Parse(Console.ReadLine()!);
+
+        // Read how many multiples to generate
+        int upto = int.Parse(Console.ReadLine()!);
+
+        // Array to store the results
+        int[] ans = new int[upto];
+
+        // Calculate multiples of n
+        for(int i = 1; i <= upto; i++)
+        {
+            ans[i - 1] = n * i;
+        }
+
+        // Print the multiplication table
+        for(int i = 0; i < upto; i++)
+        {
+            Console.Write(ans[i] + " ");
+        }
+    }
+}
