66a007a3a177d9da97ea48996233cf0c2c507d3a
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-01-19 12:39:09 +0530

    task7 done

--- Projects /TopBrains Ex/Task7/Program.cs
+++ Projects /TopBrains Ex/Task7/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static string GetHeightCategory(int heightCm)
+    {
+        if (heightCm < 150)
+            return "Short";
+        else if (heightCm < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+
+    public static void Main(string[] args)
+    {
+        int heightCm = int.Parse(Console.ReadLine()!);
+        string category = GetHeightCategory(heightCm);
+        Console.WriteLine(category);
+    }
+}
