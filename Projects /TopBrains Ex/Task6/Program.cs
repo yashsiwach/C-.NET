e02fcb74d26a79a79877eae615d262e1cbd5c043
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-01-19 12:34:18 +0530

    Task 6 Done

---
+++ Projects /TopBrains Ex/Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int a=int.Parse(Console.ReadLine()!);
+        int b=int.Parse(Console.ReadLine()!);
+        int c=int.Parse(Console.ReadLine()!);
+        if (a > b && a > c)
+        {
+            System.Console.WriteLine(a);
+        }
+        else if (b > a && b > c)
+        {
+            System.Console.WriteLine(b);
+        }
+        else
+        {
+            System.Console.WriteLine(c);
+        }
+    }
+}
