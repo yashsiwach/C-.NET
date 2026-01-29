e425333d9e63273195d69dd546d88167faff8a94
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-01-29 11:29:30 +0530

    Unit Test Done

---
+++ SelfPrograms/MyNUnitSolution/MyLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace MyLibrary.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
