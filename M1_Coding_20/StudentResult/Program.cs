b8aad136248a5512bbe84c3eb3904d0b95852962
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-02-09 11:30:29 +0530

    Solved till 7

---
+++ M1_Coding_20/StudentResult/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Person
+{
+    public string? Name{get;set;}
+    public int Age{get;set;}
+    public Person(string name,int age)
+    {
+        Name=name;
+        Age=age;
+    }
+}
+public class Student:Person
+{
+    public int RollNo{get;set;}
+    public int Marks{get;set;}
+    public Student(int roll,int marks,string name,int age) : base(name, age)
+    {
+        RollNo=roll;
+        if (marks < 35)
+        {
+            System.Console.WriteLine("fail");
+        }
+        else
+        {
+            System.Console.WriteLine("Pass");
+            System.Console.WriteLine(name+" "+age+" "+roll);
+        }
+    }
+
+}
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Student student1=new Student(1,65,"test1",23);
+        Student student2=new Student(2,30,"test2",23);
+    }
+}
