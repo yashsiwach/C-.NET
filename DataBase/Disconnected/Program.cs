77758adcb5c6ce2e6779ba43ae180e764abdbfe2
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2026-02-14 14:25:47 +0530

    Database Diconnect

---
+++ DataBase/Disconnected/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+public class Program
+{
+    public static void Main()
+    {
+        string? ConnectionString = "Data Source=localhost;Database=StudentDB;Persist Security Info=True;User ID=sa;Password=His@r143;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Command Timeout=0";
+        string? sql="select * from student";
+        using SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+        sqlConnection.Open();
+        DataSet dataSet = new();
+        SqlDataAdapter dataAdapter =  new SqlDataAdapter(sql, sqlConnection);
+        dataAdapter.Fill(dataSet);
+        foreach (DataRow row in dataSet.Tables[0].Rows)
+        {
+            Console.WriteLine(row[0]+" "+row[1]+" "+row[2]+" "+row[3]);
+        }
+        // SqlCommandBuilder cb = new SqlCommandBuilder(da);
+//
+// ds.Tables["Students"].Rows[0]["Name"] = "Yash";
+//
+// da.Update(ds, "Students");
+
+    }
+}
